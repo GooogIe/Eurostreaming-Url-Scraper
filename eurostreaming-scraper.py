249f96b9b6bb0d394c8cc5a557e52990249a0537
A Sad Loners <nameless5@protonmail.com>
2017-08-15 12:40:01 +0200

    A bit of oop

--- eurostreaming-scraper.py
+++ eurostreaming-scraper.py
@@ -3,61 +3,97 @@
 from bs4 import BeautifulSoup	# Used to get content from div could have used re though
 import cfscrape		# Bypass cloudflare
 import re			# Some regular expressions to carry out some readable data from html
+import unshortenit
 
+class Eurostreamingscraper():
+	def __init__(self,query):
+		self.query = query
 
-def getUrls(query):
-	url = "https://eurostreaming.club/?s="+query
-	scraper = cfscrape.create_scraper()
-	data = scraper.get(url).content
+	# Returns the urls found from the research issued
+	def getUrls(self):
+		url = "https://eurostreaming.club/?s="+self.query 
+		scraper = cfscrape.create_scraper()
+		data = scraper.get(url).content
 	
-	results = re.findall(r'<h2><a href="(https://eurostreaming\.club/.*?)" title="(.*?)">',data)
+		results = re.findall(r'<h2><a href="(https://eurostreaming\.club/.*?)" title="(.*?)">',data)
 	
-	return results
+		return results
 
-def getLinkspageUrl(url):
-	scraper = cfscrape.create_scraper()
-	data = scraper.get(url).content
+	# From the selected tv show url, get the link to the episodes list
+	def getLinkspageUrl(self,url):
+		scraper = cfscrape.create_scraper()
+		data = scraper.get(url).content
 
-	first_attempt = ''.join(re.findall(r'<a href="(.*?)"> &gt;&gt;',data))
-	if first_attempt == '':
-		return ''.join(re.findall(r'"go_to":"(.*?)"',data)).replace("\\","")
-	return first_attempt
+		first_attempt = ''.join(re.findall(r'<a href="(.*?)"> &gt;&gt;',data))
+		if first_attempt == '':
+			return ''.join(re.findall(r'"go_to":"(.*?)"',data)).replace("\\","")
+		return first_attempt
 
-def getData(url):
-	results = []
+	# Attempts to get the clean url to the streaming by removing adfly and others shorteners
+	def getClearUrl(self,url):
+		try:
+			un, status = unshortenit.unshorten_only(url)
+			tmp = url.split("/","")
+			tmp = url.split(".","")
+			if url != un or "vcrypt" not in tmp or "linkup" not in tmp:
+				return un
+			un, status = unshortenit.unshorten(url)
+			return un
+		except:
+			return url
 
-	scraper = cfscrape.create_scraper()
-	data = scraper.get(url).content
-	doc = BeautifulSoup(data,"lxml")
+	# Returns a list of dictionaries containing episode and its links -> {"episode_name" : name,"links": "link1 - link2 - link3"}
+	def getData(self,url):
+		results = []
+		
+		scraper = cfscrape.create_scraper()
+		data = scraper.get(url).content
+		doc = BeautifulSoup(data,"lxml")
+		seasons = doc.findAll('div', {'class': 'su-spoiler-content su-clearfix',"style":"display:none"})
+		for season in seasons:
+			season = str(season).split("<br/>")
+			for ep in season:
+				try:
+					tmplinks = re.findall(r'<a href="(.*?)"',ep)
+					links = []
+					for link in tmplinks:
+						links.append(self.getClearUrl(link))
+					
+					res = {"episode":re.findall(r'([0-9]+.*?)–',ep)[0] ,"links":" - ".join(links)}
+					results.append(res)
+				except:
+					pass
+		return results
 
-	seasons = doc.findAll('div', {'class': 'su-spoiler-content su-clearfix',"style":"display:none"})
-	for season in seasons:
-		season = str(season).split("<br/>")
-		for ep in season:
-			try:
-				string = re.findall(r'([0-9]+.*?)–',ep)[0] +" - "+" - ".join(re.findall(r'<a href="(.*?)"',ep))
-				results.append(string)
-			except:
-				pass
-	return results
+	# Returns the list of dictionaries from scratch
+	def getEpisodes(self):
+		results = self.getUrls()
 
-query = raw_input("Show/movie to look for: ")
+		if len(results) == 0:
+		 sys.exit("No results found.")
 
-results = getUrls(query)
+		for i in range(0,len(results)):
+			print (str(i+1) +" - "+results[i][1])
 
-if len(results) == 0:
- sys.exit("No artist found")
+		while True:
+			choice = int(raw_input("Type the number of the movie/show you want: "))
+			if choice>0 and choice<len(results)+1:
+				break
+			print("Invalid choice.")
 
-for i in range(0,len(results)):
-	print (str(i+1) +" - "+results[i][1])
+		print("Getting links and obtaining clear urls..This may take up some time")
 
-while True:
-	choice = int(raw_input("Type the number of the movie/show you want: "))
-	if choice>0 and choice<len(results)+1:
-		break
-	print("Invalid choice.")
+		return self.getData(self.getLinkspageUrl(results[choice-1][0]))
 
-founds = getData(getLinkspageUrl(results[choice-1][0]))
+	# Print the list of results found
+	def printEpisodes(self):
+		eps = self.getEpisodes()
+		print eps
+		for ep in eps:
+			print ep["episode"] +" - "+ep["links"]
 
-for item in founds:
-	print(item)
+
+query = raw_input("Show to look for: ")
+
+eurostreaming = Eurostreamingscraper(query)
+eurostreaming.printEpisodes()
